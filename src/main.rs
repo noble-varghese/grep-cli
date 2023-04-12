0933a2af8c34b596b62ca95039530ea56fd0e787
freebird96 <noblekvarghese96@gmail.com>
2023-04-12 14:04:05 +0530

    feat: grep-cli implementation program - intial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use grep_cli::Config;
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing the arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(err) = grep_cli::run(config) {
+        println!("Application error: {err}");
+        process::exit(1);
+    }
+}
